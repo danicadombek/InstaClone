aa71ddad57ac0fce79328b8b17d3ccee8e3f3219
Danica Dombek <danica.dombek@gmx.de>
2023-12-19 15:07:01 +0100

    Fix build issue

--- app/src/main/java/com/example/instaclone/IgViewModel.kt
+++ app/src/main/java/com/example/instaclone/IgViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.instaclone
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.instaclone.data.Event
+import com.example.instaclone.data.UserData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.lang.Exception
 import javax.inject.Inject
+
+const val USERS = "users"
 
 @HiltViewModel
 class IgViewModel @Inject constructor(
@@ -14,4 +20,41 @@ class IgViewModel @Inject constructor(
     val storage: FirebaseStorage
 ): ViewModel() {
 
+    val signedIn = mutableStateOf(false)
+    val inProgress = mutableStateOf(false)
+    val userData = mutableStateOf<UserData?>(null)
+    val popUpNotification = mutableStateOf<Event<String>?>(null)
+    fun onSignUp(userName: String, email: String, passWord: String) {
+        inProgress.value = true
+
+        db.collection(USERS).whereEqualTo("username", userName).get()
+            .addOnSuccessListener { documents ->
+                if (documents.size() > 0) {
+                    handleException(customMessage = "Username already exists")
+                    inProgress.value = false
+                } else {
+                    auth.createUserWithEmailAndPassword(email, passWord)
+                        .addOnCompleteListener {task ->
+                            if (task.isSuccessful) {
+                                signedIn.value = true
+                                handleException(task.exception, "Sign up success")
+                                // Create profile
+                            } else {
+                                handleException(task.exception, "Sign up failed")
+                            }
+                            inProgress.value = false
+                        }
+                }
+            }
+            .addOnFailureListener {  }
+    }
+
+    fun handleException(exception: Exception? = null, customMessage: String = "") {
+        exception?.printStackTrace()
+        val errorMsg = exception?.localizedMessage ?: ""
+        val message = if (customMessage.isEmpty()) errorMsg else "$customMessage : $errorMsg"
+        popUpNotification.value = Event(message)
+    }
 }
+    
+  
--- app/src/main/java/com/example/instaclone/MainActivity.kt
+++ app/src/main/java/com/example/instaclone/MainActivity.kt
@@ -10,8 +10,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.instaclone.auth.SignUpScreen
+import com.example.instaclone.main.NotificationMessage
+import com.example.instaclone.storage.StorageScreen
 import com.example.instaclone.ui.theme.InstaCloneTheme
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -30,9 +37,29 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+// sealed class für alle ziel-screens
+// TODO check sealed class
+sealed class DestinationScreen(val route: String) {
+    object SignUp: DestinationScreen("signup")
+    object Storage: DestinationScreen("storage")
+}
+
 @Composable
 fun InstagramApp() {
     val vm = hiltViewModel<IgViewModel>()
+    val navController = rememberNavController() // navi ohne parameter, gibt uns nav controller, damit nav host genutzt werden kann
+    
+    NotificationMessage(vm = vm)
+    
+    NavHost(navController = navController, startDestination = DestinationScreen.SignUp.route) {
+        composable(DestinationScreen.SignUp.route) {
+            SignUpScreen(navController = navController, vm = vm)
+        }
+        composable(DestinationScreen.Storage.route) {
+            StorageScreen(navController = navController)
+        }
+    }
 }
 
 
